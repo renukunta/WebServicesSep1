cb663f1ced88b9389f59b893d96d279e82b140d5
Swapna Renukunta <tswapnars@gmail.com>
2018-09-01 17:16:18 +1000

    first commit

--- WebApplicationCoreSep1/WebApplicationCoreSep1/Startup.cs
+++ WebApplicationCoreSep1/WebApplicationCoreSep1/Startup.cs
@@ -14,7 +14,7 @@ namespace WebApplicationCoreSep1
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        }  
 
         public IConfiguration Configuration { get; }
 
